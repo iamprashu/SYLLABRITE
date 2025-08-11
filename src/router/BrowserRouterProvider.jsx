e2b556038cf20a817bc6b1b2a82ce19d9de36797
iamprashu <iamprashu.gs@gmail.com>
2025-08-11 11:11:25 +0530

    Modified Routers

--- src/router/BrowserRouterProvider.jsx
+++ src/router/BrowserRouterProvider.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Auth from "../pages/Auth";
 import Navbar from "../components/Navbar";
 import Dashboard from "../pages/Dashboard";
 import Schedule from "../pages/Schedule";
 import Subjects from "../pages/Subjects";
 import Quiz from "../pages/Quiz";
+import { SignedIn, SignedOut } from "@clerk/clerk-react";
 
 const BrowserRouterProvider = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Auth />} />
+        <Route path="/" element={<Auth/>} />
         <Route path="/app" element={<Navbar />}>
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="schedule" element={<Schedule />} />
